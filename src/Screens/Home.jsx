eceb0be2db98f2dfc75e2a41547af754cab6cb19
jesuswav <garciajesse2003@gmail.com>
2024-04-02 21:42:56 -0600

    refactor vale status component

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -19,6 +19,8 @@ import { useNavigation } from '@react-navigation/native'
 
 import Spinner from 'react-native-loading-spinner-overlay'
 
+import ValeStatus from '../Components/ValeStatus'
+
 import useFetch from '../hooks/useFetch'
 
 const images = [
@@ -194,62 +196,7 @@ const Home = () => {
       </View>
 
       {/* Show vale status component */}
-      <View style={styles.valeStatus}>
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: '600',
-            color: 'rgba(69, 69, 69, 1)',
-          }}
-        >
-          🌻 Vale status
-        </Text>
-        <Text
-          style={{
-            fontSize: 15,
-            fontWeight: '600',
-            color: 'rgba(105, 105, 105, 1)',
-            marginTop: 8,
-          }}
-        >
-          Is the Vale open or closed?
-        </Text>
-        <Text
-          style={{
-            fontWeight: '400',
-            color: 'rgba(105, 105, 105, 1)',
-            marginTop: 4,
-          }}
-        >
-          You can see the state of the vale here in your application!
-        </Text>
-        <View
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor:
-              valeStatus === false
-                ? 'rgba(46, 139, 87, 0.6)'
-                : 'rgba(92, 201, 140, 0.9)',
-            marginTop: 18,
-            borderRadius: 18,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 18,
-              fontWeight: '500',
-              color: 'white',
-              padding: 16,
-            }}
-          >
-            {(loading && (valeStatus === false ? 'Closed' : 'Open')) ||
-              'Loading...'}
-          </Text>
-        </View>
-      </View>
+      <ValeStatus valeStatus={valeStatus} loading={loading} />
 
       <View>
         <Text
